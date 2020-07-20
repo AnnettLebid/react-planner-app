686bb629b07f811907e65ac779003efba041c582
Anna <annalez16@gmail.com>
2020-07-20 15:28:02 +0300

    Start of project

---
+++ src/components/ToDoList/ToDoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// const ToDoList = (props) => {
+//     const todos = props.todos
+//     return (
+//         <ul>
+//             {todos.map((todo, index) =>
+//             <li key = {`todo.date_${index}`}>
+//                 {todo}
+//                 </li>
+//             )}            
+//         </ul>
+//     )
+// }
+
+// export default ToDoList
